f0add99c61821cc6ba1317120e46af3cebb22f73
zamami <s1413115@gmail.com>
2023-04-11 22:49:40 +0900

    レビュー後の修正をしました。

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -8,27 +8,18 @@ opt.on( "-m ARG", desc = "set month variable" ){ |m| option[:m] = m }
 opt.on( "-y ARG", desc = "set year variable" ){ |y| option[:y] = y }
 opt.parse(ARGV)
 
-if option[:m] != nil
-  month = option[:m].to_i
-else
-  month = Date.today.month
-end
+option[:m] ? month = option[:m].to_i : month = Date.today.month
+option[:y] ? year = option[:y].to_i : year = Date.today.year
 
-if option[:y] != nil
-  year = option[:y].to_i
-else
-  year = Date.today.year
-end
+puts "       #{month}月 #{year}"
+puts "日 月 火 水 木 金 土"
 
+space  = "   "
 first_date = Date.new(year, month, 1)
 last_date = Date.new(year, month, -1)
-range_days = (first_date..last_date)
-space  = "   "
-
-puts "       #{month}月 #{year}        "
-puts "日 月 火 水 木 金 土"
-range_days.each do |range_day|
-  print space * range_day.wday if range_day.day == 1
-  print range_day.day.to_s.rjust(2," ") + " "
-  puts "\n" if range_day.wday == 6
+days_range = (first_date..last_date)
+days_range.each do |days_range|
+  print space * days_range.wday if days_range.day == 1
+  print days_range.day.to_s.rjust(2," ") + " "
+  puts "\n" if days_range.saturday?
 end
