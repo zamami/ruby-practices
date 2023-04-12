c8ce8977079eb9e7d0f8ca7291e55b3713e75e14
zamami <s1413115@gmail.com>
2023-04-12 16:36:23 +0900

    メソッドを２個追加

--- 04.ls/my-ls.rb
+++ 04.ls/my-ls.rb
@@ -18,8 +18,12 @@ Dir.foreach('.') do |file|
   all_files << file
 end
 
+def get_files_max_size(files)
+  files.max_by{|str| str.length}.size + 1
+end
+
 # lsコマンドの見た目を整えるため、配列の中から一番文字数が大きものを見つける。
-files_max_size = files.max_by{|str| str.length}.size + 1
+files_max_size = get_files_max_size(files)
 resize_files = []
 
 files.each do |file|
@@ -27,8 +31,11 @@ files.each do |file|
 end
 
 column = 3 #column: "列数"、tolerance: "公差"
-tolerance = (resize_files.size / column.to_f).ceil
+def get_tolerance(resize_files,column)
+  (resize_files.size / column.to_f).ceil
+end
 
+tolerance = get_tolerance(resize_files,column)
 colum_resize_files = resize_files.each_slice(tolerance).to_a
 colum_resize_files.each do |file| # transposeを使うために足りない要素をnilで埋める。
   (tolerance - file.size ).times{ file << nil}
