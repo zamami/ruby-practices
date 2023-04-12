5ed1db0c70889ca8caf0126a69d9b56f2e8ed654
zamami <s1413115@gmail.com>
2023-04-12 10:24:11 +0900

    再度修正をしました

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -8,18 +8,16 @@ opt.on( "-m ARG", desc = "set month variable" ){ |m| option[:m] = m }
 opt.on( "-y ARG", desc = "set year variable" ){ |y| option[:y] = y }
 opt.parse(ARGV)
 
-option[:m] ? month = option[:m].to_i : month = Date.today.month
-option[:y] ? year = option[:y].to_i : year = Date.today.year
+month = option[:m] ? option[:m].to_i : Date.today.month
+year = option[:y] ? option[:y].to_i : Date.today.year
 
 puts "       #{month}月 #{year}"
 puts "日 月 火 水 木 金 土"
-
-space  = "   "
 first_date = Date.new(year, month, 1)
+print "   " * first_date.wday
 last_date = Date.new(year, month, -1)
-days_range = (first_date..last_date)
-days_range.each do |days_range|
-  print space * days_range.wday if days_range.day == 1
-  print days_range.day.to_s.rjust(2," ") + " "
-  puts "\n" if days_range.saturday?
+date_range = (first_date..last_date)
+date_range.each do |date|
+  print date.day.to_s.rjust(2," ") + " "
+  puts "\n" if date.saturday?
 end
