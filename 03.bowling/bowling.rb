e742ab7945eb1aae14d74e68daf67151f476d581
zamami <s1413115@gmail.com>
2023-04-17 19:53:35 +0900

    レビューを受け大幅に全体的に修正しました

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -1,5 +1,5 @@
-#!/usr/bin/env ruby
-# １投ごとに分割する
+# !/usr/bin/env ruby
+
 score = ARGV[0]
 scores = score.split(',')
 
@@ -8,101 +8,24 @@ shots = []
 scores.each do |s|
   if s == 'X' # strike
     shots << 10
-    shots << 0
   else
     shots << s.to_i
   end
 end
 
-one_four_seven_ten_point_frames = []
-shots.each_slice(6) do |s|
-  one_four_seven_ten_point_frames << s
-end
-
-2.times { shots.shift }
-two_five_eight_point_frames = []
-shots.each_slice(6) do |s|
-  two_five_eight_point_frames << s
-end
-
-2.times { shots.shift }
-three_six_nine_point_frames = []
-shots.each_slice(6) do |s|
-  three_six_nine_point_frames << s
-end
-
 point = 0
-one_four_seven_ten_point_frames.each_with_index do |frame, i|
-  point += frame.sum if i == one_four_seven_ten_point_frames.length - 1
-  next unless frame.count == 6 && i != one_four_seven_ten_point_frames.length - 1
-
-  point += if frame[0] == 10 && frame[2] == 10 # ストライクの時(20~30得点)
-             frame[0] + frame[2] + frame[4]
-           elsif frame[0] == 10 # ストライクの時（10~19得点）
-             frame[0] + frame[2] + frame[3]
-           elsif frame[0] + frame[1] == 10 # スペアの時
-             frame[0] + frame[1] + frame[2]
-           else
-             frame[0] + frame[1] # ストライクでもスペアでもない
-           end
-end
-
-two_five_eight_point_frames.each_with_index do |frame, i|
-  if frame.count == 6 && i != two_five_eight_point_frames.length - 1
-    point += if frame[0] == 10 && frame[2] == 10 # ストライクの時(20~30得点)
-               frame[0] + frame[2] + frame[4]
-             elsif frame[0] == 10 # ストライクの時（10~19得点）
-               frame[0] + frame[2] + frame[3]
-             elsif frame[0] + frame[1] == 10 # スペアの時
-               frame[0] + frame[1] + frame[2]
-             else
-               frame[0] + frame[1] # ストライクでもスペアでもない
-             end
-  elsif frame.count == 6 && i == two_five_eight_point_frames.length - 1
-    point += if frame[0] == 10 && frame[2] == 10 # ストライクの時(20~30得点)
-               frame[0] + frame[2] + frame[4]
-             elsif frame[0] == 10 # ストライクの時（10~19得点）
-               frame[0] + frame[2] + frame[3]
-             elsif frame[0] + frame[1] == 10 # スペアの時
-               frame[0] + frame[1] + frame[2]
-             else
-               frame[0] + frame[1] # ストライクでもスペアでもない
-             end
+index = 0
+10.times do |frame|
+  if shots[index] == 10 # ストライクの時
+    point += shots[index..index + 2].sum
+    index += 1
+  elsif shots[index..index + 1].sum == 10 # スペアの時
+    point += shots[index..index + 2].sum
+    index += 2
+  elsif shots[index..index + 1].sum < 10 # ストライクとスペア以外
+    point += shots[index..index + 1].sum
+    index += 2
   end
 end
 
-three_six_nine_point_frames.each_with_index do |frame, i|
-  if frame.count == 6 && i <= 2
-    point += if frame[0] == 10 && frame[2] == 10 # ストライクの時(20~30得点)
-               frame[0] + frame[2] + frame[4]
-             elsif frame[0] == 10 # ストライクの時（10~19得点）
-               frame[0] + frame[2] + frame[3]
-             elsif frame[0] + frame[1] == 10 # スペアの時
-               frame[0] + frame[1] + frame[2]
-             else
-               frame[0] + frame[1] # ストライクでもスペアでもない
-             end
-  elsif frame.count == 5 && i <= 2
-    point += if frame[0] == 10 && frame[2] == 10 # ストライクの時(20~30得点)
-               frame[0] + frame[2] + frame[4]
-             elsif frame[0] == 10 # ストライクの時（10~19得点）
-               frame[0] + frame[2] + frame[3]
-             elsif frame[0] + frame[1] == 10 # スペアの時
-               frame[0] + frame[1] + frame[2]
-             else
-               frame[0] + frame[1] # ストライクでもスペアでもない
-             end
-  elsif frame.count == 4 && i <= 2
-    point += if frame[0] == 10 && frame[2] == 10 # ストライクの時(20~30得点)
-               frame[0] + frame[2] + frame[4]
-             elsif frame[0] == 10 # ストライクの時（10~19得点）
-               frame[0] + frame[2] + frame[3]
-             elsif frame[0] + frame[1] == 10 # スペアの時
-               frame[0] + frame[1] + frame[2]
-             else
-               frame[0] + frame[1] # ストライクでもスペアでもない
-             end
-  end
-end
-
-puts point
+p point
