5c4f7c71375b93816126094a5868e2125571f859
zamami <s1413115@gmail.com>
2023-04-18 10:53:12 +0900

    修正しました

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -7,29 +7,11 @@ input = ARGV
 options = ARGV.getopts('alr')
 
 # まずはディレクトリの中身を配列に格納
-files = []
-Dir.foreach('.') do |file|
-  next if file.start_with?('.')
-
-  files << file
-end
-
-all_files = []
-Dir.foreach('.') do |file|
-  all_files << file
-end
-
-def get_files_max_size(files)
-  files.max_by(&:length).size + 1
-end
+files = Dir.foreach('.').select { |file| file.start_with?(/\w/) }
 
 # lsコマンドの見た目を整えるため、配列の中から一番文字数が大きものを見つける。
-files_max_size = get_files_max_size(files)
-resize_files = []
-
-files.each do |file|
-  resize_files << file.ljust(files_max_size)
-end
+files_max_size = files.max_by(&:length).size + 1
+resize_files = files.map { |file| file.ljust(files_max_size) }
 
 column = 3 # column: "列数"、tolerance: "公差"
 def get_tolerance(resize_files, column)
