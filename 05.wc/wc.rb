a8bdffacad6828fb2c8beac67a92a576f38b89fd
zamami <s1413115@gmail.com>
2023-06-03 16:56:15 +0900

    オプション実装（total未実装)

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -1,21 +1,58 @@
 # frozen_string_literal: true
 
-files = ARGV
-total_file_symbol = { line_count: 0, word_count: 0, file_size: 0 }
-file_symbol = { line_count: 0, word_count: 0, file_size: 0 }
-output_format = '%8d%8d%8d %s'
+require 'optparse'
 
-files.each do |file|
-  text = File.read(file)
-  file_symbol[:line_count] = text.count("\n")
-  file_symbol[:word_count] = text.split(/\s+/).size
-  file_symbol[:file_size] = text.bytesize
-  puts format(output_format, file_symbol[:line_count], file_symbol[:word_count], file_symbol[:file_size], file)
+def option_wc(file_path, options)
+  line_count = 0
+  word_count = 0
+  byte_count = 0
 
-  total_file_symbol[:line_count] += text.count("\n")
-  total_file_symbol[:word_count] += text.split(/\s+/).size
-  total_file_symbol[:file_size] += text.bytesize
+  File.open(file_path, 'r') do |file|
+    file.each_line do |line|
+      line_count += 1 if options['l']
+      word_count += line.split.size if options['w']
+      byte_count += line.bytesize if options['c']
+    end
+  end
+
+  result = ''
+  result += line_count.to_s.rjust(8) if options['l']
+  result += word_count.to_s.rjust(8) if options['w']
+  result += byte_count.to_s.rjust(8) if options['c']
+  result += " #{file_path}"
+
+  puts result
 end
 
-total_output_format = '%8d%8d%8d'
-puts "#{format(total_output_format, total_file_symbol[:line_count], total_file_symbol[:word_count], total_file_symbol[:file_size])} total" if files[1]
+def not_option_wc(files)
+  file = { line_count: 0, word_count: 0, file_size: 0 }
+  total_file = { line_count: 0, word_count: 0, file_size: 0 }
+  output_format = '%8d%8d%8d %s'
+
+  files.each do |file_path|
+    text = File.read(file_path)
+    file[:line_count] = text.count("\n")
+    file[:word_count] = text.split(/\s+/).size
+    file[:file_size] = text.bytesize
+    puts format(output_format, file[:line_count], file[:word_count], file[:file_size], file_path)
+
+    total_file[:line_count] += text.count("\n")
+    total_file[:word_count] += text.split(/\s+/).size
+    total_file[:file_size] += text.bytesize
+  end
+
+  total_format = '%8d%8d%8d'
+  puts "#{format(total_format, total_file[:line_count], total_file[:word_count], total_file[:file_size])} total" if files[1]
+end
+
+files = ARGV
+options = ARGV.getopts('lcw')
+
+if options.value?(true)
+  files.each do |file|
+    option_wc(file, options)
+  end
+else
+  not_option_wc(files)
+end
+
