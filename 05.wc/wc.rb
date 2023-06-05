54269e5a9d152c652d2ce684fe2aad092c447dbd
zamami <s1413115@gmail.com>
2023-06-05 09:06:55 +0900

    ハッシュを用いた修正

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -5,80 +5,78 @@ require 'optparse'
 files = ARGV
 options = ARGV.getopts('lcw')
 
-def option_wc_total(files, total_line_count, total_word_count, total_byte_count)
+def option_wc_total(options, counts)
   result = ''
-  result += total_line_count.to_s.rjust(8) if options['l']
-  result += total_word_count.to_s.rjust(8) if options['w']
-  result += total_byte_count.to_s.rjust(8) if options['c']
+  result += counts[:line_count].to_s.rjust(8) if options['l']
+  result += counts[:word_count].to_s.rjust(8) if options['w']
+  result += counts[:byte_count].to_s.rjust(8) if options['c']
   result += ' total'
   puts result
 end
 
 def option_word_count(files, options)
-  total_line_count = 0
-  total_word_count = 0
-  total_byte_count = 0
+  total_counts = { line_count: 0, word_count: 0, byte_count: 0 }
 
   files.each do |file_path|
+    counts = { line_count: 0, word_count: 0, byte_count: 0 }
     File.open(file_path, 'r') do |file|
-      line_count = 0
-      word_count = 0
-      byte_count = 0
       file.each_line do |line|
-        line_count += 1 if options['l']
-        word_count += line.split.size if options['w']
-        byte_count += line.bytesize if options['c']
+        counts[:line_count] += 1 if options['l']
+        counts[:word_count] += line.split.size if options['w']
+        counts[:byte_count] += line.bytesize if options['c']
 
-        total_line_count += 1
-        total_word_count += line.split.size
-        total_byte_count += line.bytesize
+        total_counts[:line_count] += 1
+        total_counts[:word_count] += line.split.size
+        total_counts[:byte_count] += line.bytesize
       end
+
       result = ''
-      result += line_count.to_s.rjust(8) if options['l']
-      result += word_count.to_s.rjust(8) if options['w']
-      result += byte_count.to_s.rjust(8) if options['c']
+      result += counts[:line_count].to_s.rjust(8) if options['l']
+      result += counts[:word_count].to_s.rjust(8) if options['w']
+      result += counts[:byte_count].to_s.rjust(8) if options['c']
       result += " #{file_path}"
       puts result
     end
   end
-  option_wc_total(files, total_line_count, total_word_count, total_byte_count) if files[1]
+
+  option_wc_total(options, total_counts) if files.size > 1
 end
 
 def word_count(files)
-  file = { line_count: 0, word_count: 0, file_size: 0 }
-  total_file = { line_count: 0, word_count: 0, file_size: 0 }
   output_format = '%8d%8d%8d %s'
+  total_file = { line_count: 0, word_count: 0, file_size: 0 }
 
   files.each do |file_path|
     text = File.read(file_path)
-    file[:line_count] = text.count("\n")
-    file[:word_count] = text.split(/\s+/).size
-    file[:file_size] = text.bytesize
-    puts format(output_format, file[:line_count], file[:word_count], file[:file_size], file_path)
+    line_count = text.count("\n")
+    word_count = text.split(/\s+/).size
+    file_size = text.bytesize
 
-    total_file[:line_count] += text.count("\n")
-    total_file[:word_count] += text.split(/\s+/).size
-    total_file[:file_size] += text.bytesize
+    puts format(output_format, line_count, word_count, file_size, file_path)
+
+    total_file[:line_count] += line_count
+    total_file[:word_count] += word_count
+    total_file[:file_size] += file_size
   end
 
-  total_format = '%8d%8d%8d'
-  puts "#{format(total_format, total_file[:line_count], total_file[:word_count], total_file[:file_size])} total" if files[1]
+  if files.size > 1
+    total_format = '%8d%8d%8d'
+    puts "#{format(total_format, total_file[:line_count], total_file[:word_count], total_file[:file_size])} total"
+  end
 end
 
 def standard_input_word_count
   text = $stdin.read
-  line_count = 0
-  word_count = 0
-  byte_count = 0
-  text.each_line do |line|
-    line_count += 1
-    word_count += line.split.size
-    byte_count += line.bytesize
-  end
-  result = ''
-  result += line_count.to_s.rjust(8)
-  result += word_count.to_s.rjust(8)
-  result += byte_count.to_s.rjust(8)
+  line_count = text.lines.count
+  word_count = text.split.size
+  byte_count = text.bytesize
+
+  result = [
+    line_count.to_s.rjust(8),
+    word_count.to_s.rjust(8),
+    byte_count.to_s.rjust(8)
+  ].join
+
   puts result
 end
 
