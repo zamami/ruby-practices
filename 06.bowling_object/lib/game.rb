983e820ce6ee251bff859af42776a0bf2623c702
zamami <s1413115@gmail.com>
2023-10-05 00:58:57 +0900

    gameクラスの作成

--- 06.bowling_object/lib/game.rb
+++ 06.bowling_object/lib/game.rb
@@ -1,2 +1,42 @@
 # frozen_string_literal: true
 
+require_relative 'frame'
+
+class Game
+  def initialize(shots)
+    @shots = shots
+  end
+
+  def frame_scores
+    index = 0
+    frame_scores = []
+
+    10.times do
+      frame = Frame.new(@shots[index], @shots[index + 1])
+      frame_score = calculate_frame_score(frame, index)
+      frame_scores << frame_score
+      index += frame.strike? ? 1 : 2
+    end
+    frame_scores
+  end
+
+  def score
+    frame_scores.sum
+  end
+
+  private
+
+  def calculate_frame_score(frame, index)
+    if frame.strike?
+      Frame.new(@shots[index],@shots[index + 1],@shots[index + 2]).score
+    elsif frame.spare?
+      Frame.new(@shots[index],@shots[index + 1],@shots[index + 2]).score
+    else
+      Frame.new(@shots[index], @shots[index + 1]).score
+    end #frame.score リファクタリング
+  end
+
+end
+
+
+
