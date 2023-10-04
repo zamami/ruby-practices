70a5d05ba009c75aa3d51253d0e84c85fb72f03a
zamami <s1413115@gmail.com>
2023-10-04 18:34:24 +0900

    shotクラスのif文を三項演算子に修正

--- 06.bowling_object/lib/shot.rb
+++ 06.bowling_object/lib/shot.rb
@@ -1,14 +1,12 @@
 # frozen_string_literal: true
 
 class Shot
-  attr_reader :mark
 
   def initialize(mark)
     @mark = mark
   end
 
   def score
-    return 10 if mark == 'X'
-    mark.to_i
+    @mark == 'X' ? 10 : @mark.to_i
   end
 end
