a7093d321dec9c2daec62422c3d63735f7b28e11
zamami <s1413115@gmail.com>
2023-10-05 00:55:26 +0900

    frameクラスのメソッドとテスト追加

--- 06.bowling_object/spec/frame_spec.rb
+++ 06.bowling_object/spec/frame_spec.rb
@@ -3,8 +3,16 @@
 require_relative '../lib/frame'
 
 RSpec.describe Frame do
-  it 'フレームの計算' do
-    expect(Frame.new("1","8").score).to eq 9
-    expect(Frame.new("X","0").score).to eq 10
+  it 'フレームのスコア' do
+    expect(Frame.new('1','8').score).to eq 9
+    expect(Frame.new('X').score).to eq 10
+  end
+
+  it 'ストライク？' do
+    expect(Frame.new('X').strike?).to eq true
+  end
+
+  it 'スペア？' do
+    expect(Frame.new('2','8').score).to eq 10
   end
 end
