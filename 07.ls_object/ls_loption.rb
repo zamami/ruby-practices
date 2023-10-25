1a4b09e833efb30bb24c5ba3888bf4f6fcef1bb7
zamami <s1413115@gmail.com>
2023-10-25 23:15:21 +0900

    一部のメソッドをprivateメソッドに変更

--- 07.ls_object/ls_loption.rb
+++ 07.ls_object/ls_loption.rb
@@ -3,23 +3,6 @@
 require 'etc'
 class LsLoption < LsCommand
   # -lオプションに対応。
-  def get_ftype(ftype)
-    if ftype == 'directory'
-      'd'
-    elsif ftype == 'file'
-      '-'
-    else
-      'l'
-    end
-  end
-
-  FILE_MODE = { '7' => 'rwx', '6' => 'rwx', '5' => 'r-x', '4' => 'r--', '3' => '-wx', '2' => '-w-', '1' => '--x', '0' => '---' }.freeze
-  def get_file_mode(stat_mode)
-    get_file_mode_num = stat_mode.to_s(8).split('').slice(-3..-1) # 下３桁を取得。
-    change_file_mode_num = get_file_mode_num.map { |num| FILE_MODE[num] }
-    change_file_mode_num.join
-  end
-
   def show
     stat_blocks_total = []
     lists_values = @lists.map do |list|
@@ -38,5 +21,24 @@ class LsLoption < LsCommand
     end
     puts "total #{stat_blocks_total.sum}"
     lists_values.each { |list| puts list }
+  end
+
+  private
+
+  def get_ftype(ftype)
+    if ftype == 'directory'
+      'd'
+    elsif ftype == 'file'
+      '-'
+    else
+      'l'
+    end
+  end
+
+  FILE_MODE = { '7' => 'rwx', '6' => 'rwx', '5' => 'r-x', '4' => 'r--', '3' => '-wx', '2' => '-w-', '1' => '--x', '0' => '---' }.freeze
+  def get_file_mode(stat_mode)
+    get_file_mode_num = stat_mode.to_s(8).split('').slice(-3..-1) # 下３桁を取得。
+    change_file_mode_num = get_file_mode_num.map { |num| FILE_MODE[num] }
+    change_file_mode_num.join
   end
 end
