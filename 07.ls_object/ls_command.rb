1bc741464185974c9158bd5a2dbe82d96626e093
zamami <s1413115@gmail.com>
2023-11-17 16:09:16 +0900

    変数名を修正しました。

--- 07.ls_object/ls_command.rb
+++ 07.ls_object/ls_command.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
 class LsCommand
-  def initialize(lists)
-    @lists = lists
+  def initialize(list)
+    @list = list
   end
 
   def show
-    lists_max_size = @lists.max_by(&:length).size + 1 # ls,ls -arコマンドの見た目を整えるため、配列の中から一番文字数が大きものを見つける。
-    resize_lists = @lists.map { |list| list.ljust(lists_max_size) }
+    lists_max_size = @list.max_by(&:length).size + 1 # ls,ls -arコマンドの見た目を整えるため、配列の中から一番文字数が大きものを見つける。
+    resize_lists = @list.map { |list| list.ljust(lists_max_size) }
     column = 3 # column: "列数"
     tolerance = (resize_lists.size / column.to_f).ceil # tolerance: "公差"
     slice_lists = resize_lists.each_slice(tolerance).to_a
