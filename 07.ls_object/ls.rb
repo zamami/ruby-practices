8f12966378683f41636cab494a31bbe6c9ff94b9
zamami <s1413115@gmail.com>
2023-11-28 21:26:27 +0900

    変数の中身がクラスインスタンスなので変数名をそのままクラス名にした。

--- 07.ls_object/ls.rb
+++ 07.ls_object/ls.rb
@@ -4,6 +4,6 @@ require_relative 'ls_parser'
 require_relative 'ls_normal_format'
 require_relative 'ls_long_format'
 
-parser = LsParser.new
-filenames = parser.filenames
-parser.l_option? ? LsLongFormat.new(filenames).show : LsNormalFormat.new(filenames).show
+ls_parser = LsParser.new
+filenames = ls_parser.format_filenames
+ls_parser.l_option? ? LsLongFormat.new(filenames).show : LsNormalFormat.new(filenames).show
