54224ed4edb96fb5699705af93bb699be4456780
zamami <s1413115@gmail.com>
2023-11-28 21:29:43 +0900

    ・変数名を単数系、正しい文法になるように修正

--- 07.ls_object/ls_normal_format.rb
+++ 07.ls_object/ls_normal_format.rb
@@ -6,11 +6,11 @@ class LsNormalFormat
   end
 
   def show
-    max_filenames_size = @filenames.max_by(&:length).size + 1 # ls,ls -arコマンドの見た目を整えるため、配列の中から一番文字数が大きものを見つける。
-    resized_filenames = @filenames.map { |filename| filename.ljust(max_filenames_size) }
+    max_filename_size = @filenames.max_by(&:length).size + 1 # ls,ls -arコマンドの見た目を整えるため、配列の中から一番文字数が大きものを見つける。
+    resized_filenames = @filenames.map { |filename| filename.ljust(max_filename_size) }
     column = 3 # column: "列数"
     tolerance = (resized_filenames.size / column.to_f).ceil # tolerance: "公差"
-    filenames_slice = resized_filenames.each_slice(tolerance).to_a
-    filenames_slice.each { |filename| (tolerance - filename.size).times { filename << nil } }.transpose.each { |filename| puts filename.join } # transposeを使うために足りない要素をnilで埋める。
+    sliced_filenames = resized_filenames.each_slice(tolerance).to_a
+    sliced_filenames.each { |filename| (tolerance - filename.size).times { filename << nil } }.transpose.each { |filename| puts filename.join } # transposeを使うために足りない要素をnilで埋める。
   end
 end
