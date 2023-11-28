da364bbd406c866189469008631db5fddd6c55db
zamami <s1413115@gmail.com>
2023-11-28 21:30:54 +0900

    ・メソッド名を動詞から始まるように修正。 ・変数名の統一しました。 ・空行の追加。

--- 07.ls_object/ls_parser.rb
+++ 07.ls_object/ls_parser.rb
@@ -1,15 +1,16 @@
 # frozen_string_literal: true
 
 require 'optparse'
+
 class LsParser
   def initialize
     @options = ARGV.getopts('alr')
   end
 
-  def filenames
-    args = ['*']
-    args << File::FNM_DOTMATCH if @options['a']
-    @options['r'] ? Dir.glob(*args).sort.reverse : Dir.glob(*args).sort
+  def format_filenames
+    filenames = ['*']
+    filenames << File::FNM_DOTMATCH if @options['a']
+    @options['r'] ? Dir.glob(*filenames).sort.reverse : Dir.glob(*filenames).sort
   end
 
   def l_option?
